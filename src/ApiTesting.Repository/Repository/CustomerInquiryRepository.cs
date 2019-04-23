c1d2cca839512afbb3bd64108c98b2faef1fb6b5
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-24 05:10:45 +0700

    add entities model and create repository class

---
+++ src/ApiTesting.Repository/Repository/CustomerInquiryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApiTesting.Repository.EntityModel;
+
+namespace ApiTesting.Repository.Repository
+{
+    class CustomerInquiryRepository
+    {
+        public List<Customer> GetData(int customerId, string customerEmail)
+        {
+            var customers = new List<Customer>();
+            using (var context = new ApiTestingEntities())
+            {
+                customers = context.Customers.Where(x => x.Customer_ID == customerId || x.Contact_Email == customerEmail).ToList();
+            }
+            return customers;
+        }
+    }
+}
