115179a12032953832f9027c075ac0849d9c04c5
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-24 08:01:43 +0700

    update data type in entity from change column type in database

--- sql/generate script/1-customers.sql
+++ sql/generate script/1-customers.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Customers(
-	Customer_ID numeric(10,0) IDENTITY(1,1) CONSTRAINT PK_Customer PRIMARY KEY,
+	Customer_ID int IDENTITY(1,1) CONSTRAINT PK_Customer PRIMARY KEY,
 	Customer_Name nvarchar(30),
 	Contact_Email nvarchar(25),
 	Mobile_No nvarchar(10)
--- sql/generate script/2-transactions.sql
+++ sql/generate script/2-transactions.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Transactions (
     Transaction_ID int IDENTITY(1,1) CONSTRAINT PK_Transaction PRIMARY KEY,
     Transaction_Date datetime Default GetDate(),
-    Customer_ID numeric(10,0) NOT NULL,
+    Customer_ID int NOT NULL,
     Amount dec(8,2),
 	Currency_Code varchar(3),
 	[Status] tinyint,
-	CONSTRAINT FK_CustomerID FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+	CONSTRAINT FK_CustomerID FOREIGN KEY (Customer_ID) REFERENCES Customers(Customer_ID)
 );
