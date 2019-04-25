2325752513958b20a3deeadc50ccf6080954db6c
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-25 13:18:04 +0700

    add testing data script

---
+++ sql/testing data.sql
@@ -0,0 +1,11 @@
+insert into Customers (Customer_Name ,Contact_Email ,Mobile_No)
+values ('Joe Gerien', 'joegerien@gmail.com', '0865175253')
+
+insert into Customers (Customer_Name ,Contact_Email ,Mobile_No)
+values ('Phongpheera Kaewprajan', 'phongpheera.k@gmail.com', '0945579573')
+
+insert into Transactions ( Customer_ID, Amount, Currency_Code, [Status])
+values (1, 100, 'THB', 1)
+
+insert into Transactions ( Customer_ID, Amount, Currency_Code, [Status])
+values (1, 0.87, 'USD', 1)
