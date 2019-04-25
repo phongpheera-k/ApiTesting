9a90154a7edd83885f4f816b37301e48d7943f80
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-25 13:56:37 +0700

    create test case not match

--- src/ApiTesting.UnitTests/Controllers/CustomerInquiryControllerTest.cs
+++ src/ApiTesting.UnitTests/Controllers/CustomerInquiryControllerTest.cs
@@ -9,10 +9,11 @@ namespace ApiTesting.UnitTests.Controllers
     public class CustomerInquiryControllerTest
     {
         [Test]
-        [TestCase("1", "joegerian@gmail.com")]
-        [TestCase(null, "joegerian@gmail.com")]
+        [TestCase("1", "joegerien@gmail.com")]
+        [TestCase("2", "joegerien@gmail.com")]
+        [TestCase(null, "joegerien@gmail.com")]
         [TestCase(2, "phongpheera.k@gmail.com")]
-        public void Post_Success(string customerID, string email)
+        public void Post_IsSuccess(string customerID, string email)
         {
             CustomerInquiryController controller = new CustomerInquiryController();
 
@@ -23,6 +24,23 @@ namespace ApiTesting.UnitTests.Controllers
             var result = controller.Post(request);
 
             Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [TestCase("0", "")]
+        [TestCase(null, null)]
+        [TestCase(2, "phong@gmail.com")]
+        public void Post_NotSuccess(string customerID, string email)
+        {
+            CustomerInquiryController controller = new CustomerInquiryController();
+
+            var request = new CustomerInquiryRequest();
+            request.customerID = customerID;
+            request.email = email;
+
+            var result = controller.Post(request);
+
+            Assert.IsNull(result);
         }
     }
 }
