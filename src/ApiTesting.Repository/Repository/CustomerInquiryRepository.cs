a2fb872edd1df677d848c307bdbd1069042e13e1
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-25 08:27:49 +0700

    change Repository to return dataobject

--- src/ApiTesting.Repository/Repository/CustomerInquiryRepository.cs
+++ src/ApiTesting.Repository/Repository/CustomerInquiryRepository.cs
@@ -2,19 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using ApiTesting.Repository.EntityModel;
+using ApiTesting.Domain.CustomerInquiry;
+using System.Reflection;
 
 namespace ApiTesting.Repository.Repository
 {
-    class CustomerInquiryRepository
+    public class CustomerInquiryRepository : ICustomerInquiryRepository
     {
-        public List<Customer> GetData(int customerId, string customerEmail)
+        public CustomerModel GetData(int customerId, string customerEmail)
         {
-            var customers = new List<Customer>();
+            var customer = new Customer();
+            var transactions = new List<Transaction>();
             using (var context = new ApiTestingEntities())
             {
-                customers = context.Customers.Where(x => x.Customer_ID == customerId || x.Contact_Email == customerEmail).ToList();
+                customer = context.Customers.Where(x => x.Customer_ID == customerId && x.Contact_Email == customerEmail).FirstOrDefault();
+                transactions = customer.Transactions.ToList();
             }
-            return customers;
+            var result = ConvertEntityCustomerToBusinessObject(customer, new CustomerModel());
+            result.Transaction = new List<TransactionModel>();
+            foreach(var tran in transactions)
+            {
+                result.Transaction.Add(ConvertEntityTransactionToBusinessObject(tran, new TransactionModel()));
+            }
+            return result;
+        }
+
+        private CustomerModel ConvertEntityCustomerToBusinessObject(Customer entityObject, CustomerModel businessObject)
+        {
+            Type BusinessObjectType = businessObject.GetType();
+            PropertyInfo[] BusinessPropList = BusinessObjectType.GetProperties();
+
+            Type EntityObjectType = entityObject.GetType();
+            PropertyInfo[] EntityPropList = EntityObjectType.GetProperties();
+
+            foreach (PropertyInfo businessPropInfo in BusinessPropList)
+            {
+                foreach (PropertyInfo entityPropInfo in EntityPropList)
+                {
+                    if (entityPropInfo.Name == businessPropInfo.Name)
+                    {
+                        businessPropInfo.SetValue(businessObject, entityPropInfo.GetValue(entityObject, null), null);
+                        break;
+                    }
+                }
+            }
+
+            return businessObject;
+        }
+
+        private TransactionModel ConvertEntityTransactionToBusinessObject(Transaction entityObject, TransactionModel businessObject)
+        {
+            Type BusinessObjectType = businessObject.GetType();
+            PropertyInfo[] BusinessPropList = BusinessObjectType.GetProperties();
+
+            Type EntityObjectType = entityObject.GetType();
+            PropertyInfo[] EntityPropList = EntityObjectType.GetProperties();
+
+            foreach (PropertyInfo businessPropInfo in BusinessPropList)
+            {
+                foreach (PropertyInfo entityPropInfo in EntityPropList)
+                {
+                    if (entityPropInfo.Name == businessPropInfo.Name)
+                    {
+                        businessPropInfo.SetValue(businessObject, entityPropInfo.GetValue(entityObject, null), null);
+                        break;
+                    }
+                }
+            }
+
+            return businessObject;
         }
     }
 }
