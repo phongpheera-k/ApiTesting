f6888b05d830b91f7577fe536c3a7f26b13627f5
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-25 13:05:53 +0700

    create unit test for new controller

---
+++ src/ApiTesting.UnitTests/Controllers/CustomerInquiryControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using ApiTesting.Models;
+
+namespace ApiTesting.UnitTests.Controllers
+{
+    [TestClass]
+    public class CustomerInquiryControllerTest
+    {
+        [TestMethod]
+        [TestCase("1", "joegerian@gmail.com")]
+        [TestCase(null, "joegerian@gmail.com")]
+        [TestCase(2, "phongpheera.k@gmail.com")]
+        public void Post_Success(string customerID, string email)
+        {
+            CustomerInquiryControllerTest controller = new CustomerInquiryControllerTest();
+
+            //var result = controller.post 
+        }
+    }
+}
