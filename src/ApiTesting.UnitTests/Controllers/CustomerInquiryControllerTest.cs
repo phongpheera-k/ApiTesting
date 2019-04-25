950746e54c0a0ec8a6bd438931d156f16adc1f57
Phongpheera Kaewprajan <phongpheera.k@gmail.com>
2019-04-25 16:30:21 +0700

    Update Unit test have request is valid and Invalid case

--- src/ApiTesting.UnitTests/Controllers/CustomerInquiryControllerTest.cs
+++ src/ApiTesting.UnitTests/Controllers/CustomerInquiryControllerTest.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
+using ApiTesting.Services.Interface;
+using ApiTesting.Services.Models;
 using ApiTesting.Models;
 using ApiTesting.Controllers;
 
@@ -12,35 +19,86 @@ namespace ApiTesting.UnitTests.Controllers
         [TestCase("1", "joegerien@gmail.com")]
         [TestCase("2", "joegerien@gmail.com")]
         [TestCase(null, "joegerien@gmail.com")]
-        [TestCase(2, "phongpheera.k@gmail.com")]
-        public void Post_IsSuccess(string customerID, string email)
+        [TestCase("2", "phongpheera.k@gmail.com")]
+        public void Post_RequestIsValid_ResponseOK(string customerID, string email)
         {
-            CustomerInquiryController controller = new CustomerInquiryController();
+            CustomerInquiryController controller = new CustomerInquiryController(FakeCustomerInquityServiceForPost(5));
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
 
-            var request = new CustomerInquiryRequest();
-            request.customerID = customerID;
-            request.email = email;
+            var request = new CustomerInquiryRequest()
+            {
+                customerID = customerID,
+                email = email
+            };
 
             var result = controller.Post(request);
 
-            Assert.IsNotNull(result);
+            Assert.That(result.StatusCode == HttpStatusCode.OK);
         }
 
         [Test]
-        [TestCase("0", "")]
+        [TestCase("", "")]
         [TestCase(null, null)]
-        [TestCase(2, "phong@gmail.com")]
-        public void Post_NotSuccess(string customerID, string email)
+        [TestCase("", null)]
+        [TestCase(null, "")]
+        [TestCase("test", null)]
+        public void Post_RequestIsInvalid_ResponseBadRequest(string customerID, string email)
         {
-            CustomerInquiryController controller = new CustomerInquiryController();
+            CustomerInquiryController controller = new CustomerInquiryController(FakeCustomerInquityServiceForPost(5));
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
 
-            var request = new CustomerInquiryRequest();
-            request.customerID = customerID;
-            request.email = email;
+            var request = new CustomerInquiryRequest()
+            {
+                customerID = customerID,
+                email = email
+            };
 
             var result = controller.Post(request);
 
-            Assert.IsNull(result);
+            Assert.That(result.StatusCode == HttpStatusCode.BadRequest); ;
+        }
+
+        private ICustomerInquiryService FakeCustomerInquityServiceForPost(int transactionRow)
+        {
+            var customerInquityService = Substitute.For<ICustomerInquiryService>();
+
+            var customerDto = new CustomerDto();
+            customerDto = GenerateFakeCustomerDto();
+            for(var i = 0; i < transactionRow; i++)
+            {
+                customerDto.transactions.Add(GenerateFakeTransactionDto());
+            }
+
+            customerInquityService.GetCustomerTransaction(Arg.Any<int>(), Arg.Any<string>())
+                                   .Returns<CustomerDto>(customerDto);
+
+            return customerInquityService;
+        }
+
+        private CustomerDto GenerateFakeCustomerDto()
+        {
+            return new CustomerDto()
+            {
+                customerID = 7,
+                name = "David Michael",
+                email = "TestEmail@Tesla.com",
+                mobile = "0812234567",
+                transactions = new List<TransactionDto>()
+            };
+        }
+
+        private TransactionDto GenerateFakeTransactionDto()
+        {
+            return new TransactionDto()
+            {
+                id = 77,
+                date = new DateTime(),
+                amount = 150,
+                currency = "THB",
+                status = "Success"
+            };
         }
     }
 }
